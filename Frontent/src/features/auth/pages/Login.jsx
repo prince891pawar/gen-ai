6a8ef47453658b27a03aa486543408ee90616e2d
prince pawar <princepawar891@gmail.com>
2026-03-16 21:55:05 +0530

    create-pages

--- Frontent/src/features/auth/pages/Login.jsx
+++ Frontent/src/features/auth/pages/Login.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
+import "./login.scss"
 
 const Login = () => {
   return (
-    <div>
-      login
-    </div>
+   <main>
+   <div className='form-container'>
+    <h1>Login</h1>
+    <form>
+      <div className="input-group">
+        <label htmlFor="email">email</label>
+          <input type="email" placeholder='enter email' />
+      </div>
+      <div className="input-group">
+       <label htmlFor="password">password</label>
+          <input type="password" placeholder='enter Password' />
+      </div>
+      <button className='button'>Login</button>
+    </form>
+   </div>
+   </main>
   )
 }
 
